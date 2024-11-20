f7213ccedd7f60b78c2df4e35468b70b0cc404ac
Quinn <austinq23@student.jhs.net>
2024-11-20 15:56:38 -0800

    firebase config + authservice

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhB915KXjXc4zG-vOxrRXbmXanzro4jkE",
+  authDomain: "fanasty-faceoff.firebaseapp.com",
+  projectId: "fanasty-faceoff",
+  storageBucket: "fanasty-faceoff.firebasestorage.app",
+  messagingSenderId: "1053527790144",
+  appId: "1:1053527790144:web:ff5f388d4f5f34e355c5a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
